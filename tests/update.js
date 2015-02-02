01249fa2a23228016916da45e202f106a6ae5bf1
Max Nachlinger <max.nachlinger@gmail.com>
2015-02-02 14:32:38 -0800

    Refactoring

--- tests/update.js
+++ tests/update.js
@@ -3,16 +3,18 @@ var test = require('tape');
 var update = require('../lib/update');
 
 test('Constructs a simple update using $where', function (t) {
-	var item = {name: 'Test', color: 'Blue', $where: 'id = 1'};
+	var item = {name: 'Test', color: 'Blue'};
 	var expectedSQL = 'UPDATE test SET name = $1, color = $2 WHERE id = 1';
 	var expectedValues = ['Test', 'Blue'];
 	
 	update({
 		item: item,
+		where: 'id = 1',
 		tableName: 'test'
 	}, function(err, res) {
-		t.equal(res.sql, expectedSQL, "Returns the expected SQL");
-		t.deepEqual(res.values, expectedValues, "Return the expected values");
+		t.notOk(err, "No error should have been returned, received: " + (err && err.stack));
+		t.equal(res.sql, expectedSQL, "Returns the expected SQL: " + res.sql);
+		t.deepEqual(res.values, expectedValues, "Return the expected values: " + JSON.stringify(res.values));
 		t.end();
 	});
 });
@@ -27,8 +29,9 @@ test('Constructs a simple update using keyColumnName', function (t) {
 		tableName: 'test',
 		keyColumnName: 'id'
 	}, function(err, res) {
-		t.equal(res.sql, expectedSQL, "Returns the expected SQL");
-		t.deepEqual(res.values, expectedValues, "Return the expected values");
+		t.notOk(err, "No error should have been returned, received: " + (err && err.stack));
+		t.equal(res.sql, expectedSQL, "Returns the expected SQL: " + res.sql);
+		t.deepEqual(res.values, expectedValues, "Return the expected values: " + JSON.stringify(res.values));
 		t.end();
 	});
 });
